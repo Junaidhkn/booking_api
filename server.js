106acf2ce06637b01bdb0af46dec893f747e3c77
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-16 21:22:31 +0500

    commit

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from './routes/auth.js'
+import hotelRoute from './routes/hotels.js'
+import roomRoute from './routes/rooms.js'
+import userRoute from './routes/users.js'
 
 
 const app = express();
 dotenv.config()
+
+
+
+app.use( '/api/auth', authRoute )
+app.use( '/api/users', userRoute )
+app.use( '/api/hotels', hotelRoute )
+app.use( '/api/rooms', roomRoute )
+
+
+
 
 const connect = async () => {
    try {
