9d1be39cdaaf005380c8ad7c1fda56661f37df73
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-18 01:37:41 +0500

    register & login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,32 @@
-export const register = async ( req, res, next ) => {
-   // try {
+import User from "../models/User.js"
+import bcrypt from 'bcryptjs'
+import { errorHandler } from "../utils/error.js"
 
-   // } catch ( err ) {
-   //    next( err )
-   // }
+
+
+export const register = async ( req, res, next ) => {
+   try {
+      const salt = bcrypt.genSaltSync( 10 )
+      const hashedPassword = bcrypt.hashSync( req.body.password, salt )
+      const newUser = new User( {
+         username: req.body.username,
+         email: req.body.email,
+         password: hashedPassword
+      } )
+      await newUser.save()
+      res.status( 201 ).send( 'User Has Been Created' )
+   } catch ( err ) {
+      next( err )
+   }
+}
+export const login = async ( req, res, next ) => {
+   try {
+      const user = User.findOne( { username: req.body.username } )
+      if ( !user ) return next( errorHandler( 404, "User Not Found!" ) )
+      const isPasswordCorrect = await bcrypt.compare( req.body.password, user.password )
+      if ( !isPasswordCorrect ) return next( errorHandler( 400, "Wrong Password or Username!" ) )
+      res.status( 200 ).json( user )
+   } catch ( err ) {
+      next( err )
+   }
 } 
