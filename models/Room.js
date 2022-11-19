335e15539045895adb07fb56d94c407b72c1218e
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-19 23:49:54 +0500

    fixes

--- models/Room.js
+++ models/Room.js
@@ -2,53 +2,23 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const RoomSchema = new Schema( {
-   name: {
+   title: {
       type: String,
       required: true
    },
-   type: {
-      type: String,
-      required: true
-   },
-   city: {
-      type: String,
-      required: true
-   },
-   address: {
-      type: String,
-      required: true
-   },
-   distance: {
-      type: String,
-      required: true
-   },
-   photos: {
-      type: [String]
-   },
-   distance: {
-      type: String,
+   price: {
+      type: Number,
       required: true
    },
    desc: {
       type: String,
       required: true
    },
-   rating: {
-      type: Number,
-      min: 0,
-      max: 5
-   },
-   rooms: {
-      type: [String]
-   },
-   cheapestPrice: {
+   maxPeople: {
       type: Number,
       required: true
    },
-   featured: {
-      type: Boolean,
-      default: false
-   }
+   roomNumbers: [{ number: Number, unavailabaleDates: { type: [Date] } }]
 
 } );
 
