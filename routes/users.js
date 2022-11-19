c3e04f9b8c1adab34b13266a21dee0b3d88bde25
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-19 21:02:00 +0500

    user routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,24 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/User.js";
 
 const router = express.Router()
 
+// CREATE
+router.post( '/', createUser )
+
+
+// UPDATE
+router.put( '/:id', updateUser )
+
+// DELETE
+router.delete( '/:id', deleteUser )
+
+// GET
+
+router.get( '/:id', getUser )
+// GET ALL
+
+router.get( '/', getUsers )
 
 
 
