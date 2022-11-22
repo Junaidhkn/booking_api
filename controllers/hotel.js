45e58ff9cd70262ee81779818d494fb0bbecb534
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-22 22:00:30 +0500

    fetch by type

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,4 +1,5 @@
 import Hotel from '../models/Hotel.js'
+import { errorHandler } from '../utils/error.js'
 
 
 // Create
@@ -73,8 +74,18 @@ export const countByCity = async ( req, res, next ) => {
 // Get All by Type
 export const countByType = async ( req, res, next ) => {
    try {
-      const hotels = await Hotel.find()
-      res.status( 200 ).json( hotels )
+      const hotelCount = await Hotel.countDocuments( { type: 'Hotel' } )
+      const apartmentCount = await Hotel.countDocuments( { type: 'Apartment' } )
+      const resortCount = await Hotel.countDocuments( { type: 'Resort' } )
+      const villaCount = await Hotel.countDocuments( { type: 'Villa' } )
+      const cabinCount = await Hotel.countDocuments( { type: 'Cabin' } )
+      res.status( 200 ).json( [
+         { type: 'Hotel', count: hotelCount },
+         { type: 'Apartment', count: apartmentCount },
+         { type: 'Resort', count: resortCount },
+         { type: 'Villa', count: villaCount },
+         { type: 'Cabin', count: cabinCount }
+      ] )
    } catch ( err ) {
       next( err )
    }
