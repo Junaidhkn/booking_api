a3bd4ef7ce5787357ae4e6842a3f1e9b79ef28f3
Junaidhkn <junaidhkniazi@gmail.com>
2022-11-28 00:36:20 +0500

    commit

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -38,7 +38,7 @@ export const deleteHotel = async ( req, res, next ) => {
 // Get one
 export const getHotel = async ( req, res, next ) => {
    try {
-      const hotel = await Hotel.findByd( req.params.id )
+      const hotel = await Hotel.findById( req.params.id )
       res.status( 200 ).json( hotel )
    } catch ( err ) {
       next( err )
